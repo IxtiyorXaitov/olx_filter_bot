94cf62edc2efaa1d62e2fb4572ec658268b797e0
IkhtiyorDev <ikhtiyordev@gmail.com>
2022-02-17 16:10:40 +0500

    first commit

---
+++ src/main/java/dev/ikhtiyor/olxfilterbot/OlxFilterBotApplication.java
@@ -0,0 +1,13 @@
+package dev.ikhtiyor.olxfilterbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlxFilterBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlxFilterBotApplication.class, args);
+    }
+
+}
